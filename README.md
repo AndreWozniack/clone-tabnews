72e41c4ee1073dbcadbdcbf6abafb410b12fefd3
André <andre_fabriciow@hotmail.com>
2025-12-02 14:34:00 -0300

    Added  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do Tabnews para o curso.dev, com implementação de fora a fora
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Duda, eu te amo, você é a mulher da minha vida! Obrigado por me apoiar tanto e em tudo!!</h1>
+  return (
+    <h1>
+      Duda, eu te amo, você é a mulher da minha vida! Obrigado por me apoiar
+      tanto e em tudo!!
+    </h1>
+  );
 }
 
 export default Home;
